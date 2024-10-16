0f92f6abf763b1c4e60ce91d8fdfb71b94d2c7ba
ashivji1 <ashivji@gmail.com>
2024-10-15 21:28:36 -0500

    Add files via upload

--- code/ch3/figure_3-7/figure_3-7.R
+++ code/ch3/figure_3-7/figure_3-7.R
@@ -1,111 +1,183 @@
-###Table 3.5###
+# Preliminaries
+chapter <- "ch3"
+title <- "figure_3-7"
+dir_root <- "C:/Users/ashiv/OneDrive/Documents/Wodtke/Causal Mediation Analysis Book/Programming/Programs/Replication"
+dir_log <- paste0(dir_root, "/code/", chapter, "/_LOGS")
+log_path <- paste0(dir_log, "/", title, "_log.txt")
+dir_fig <- paste0(dir_root, "/figures/", chapter)
 
-rm(list=ls())
+# Open log
+sink(log_path, split = TRUE)
+#-------------------------------------------------------------------------------
+# Causal Mediation Analysis Replication Files
 
-packages<-c("dplyr", "tidyr", "foreach", "doParallel", "doRNG", "ggplot2")
+# GitHub Repo: https://github.com/causalMedAnalysis/repFiles/tree/main
 
-#install.packages(packages)
+# Script:      .../code/ch3/figure_3-7.R
 
-for (package.i in packages) {
-	suppressPackageStartupMessages(library(package.i, character.only=TRUE))
-	}
+# Inputs:      https://raw.githubusercontent.com/causalMedAnalysis/repFiles/refs/heads/main/data/NLSY79/nlsy79BK_ed2.dta
+#              https://raw.githubusercontent.com/causalMedAnalysis/causalMedR/refs/heads/main/utils.R
+#              https://raw.githubusercontent.com/causalMedAnalysis/causalMedR/refs/heads/main/ipwmed.R
 
-nboot <- 2000
+# Outputs:     .../code/ch3/_LOGS/figure_3-7_log.txt
+#              .../figures/ch3/figure_3-7.png
 
-ncores <- parallel::detectCores()-1
-
-##office
-datadir <- "C:/Users/Geoffrey Wodtke/Dropbox/shared/causal_mediation_text/data/" 
-logdir <- "C:/Users/Geoffrey Wodtke/Dropbox/shared/causal_mediation_text/code/ch3/_LOGS/"
-figdir <- "C:/Users/Geoffrey Wodtke/Dropbox/shared/causal_mediation_text/figures/ch3/"
-
-##home
-#datadir <- "C:/Users/Geoff/Dropbox/shared/causal_mediation_text/data/" 
-#logdir <- "C:/Users/Geoff/Dropbox/shared/causal_mediation_text/code/ch3/_LOGS/"
-#figdir <- "C:/Users/Geoff/Dropbox/shared/causal_mediation_text/figures/ch3/"
-
-sink(paste(logdir, "figure_3-7_log.txt", sep=""))
-
-##input data 
-nlsy <- as.data.frame(readRDS(paste(datadir, "NLSY79/nlsy79BK_ed2.RDS", sep="")))
-
-nlsy <- nlsy[complete.cases(nlsy[,c("cesd_age40", "ever_unemp_age3539", "att22", "female", "black", "hispan", "paredu", "parprof", "parinc_prank", "famsize", "afqt3")]),]
-
-nlsy$std_cesd_age40 <- (nlsy$cesd_age40-mean(nlsy$cesd_age40))/sd(nlsy$cesd_age40)
-
-##inverse probability weighting estimators
-
-#define functions for estimators
-
-ipwmed <- function(data) {
-	
-	df <- data
-
-	Dmodel_1 <- glm(att22~female+black+hispan+paredu+parprof+parinc_prank+famsize+afqt3, data=df, family=binomial("logit"))
-
-	Dmodel_2 <- glm(att22~ever_unemp_age3539+female+black+hispan+paredu+parprof+parinc_prank+famsize+afqt3, data=df, family=binomial("logit"))
-
-	df$phat_d_C <- predict(Dmodel_1, type = "response")
-	df$phat_dstar_C <- 1-df$phat_d_C
-
-	df$phat_d_CM <- predict(Dmodel_2, type = "response")
-	df$phat_dstar_CM <- 1-df$phat_d_CM
-
-	df$phat_d <- mean(df$att22)
-	df$phat_dstar <- 1-df$phat_d
-
-	df$sw1[df$att22==0] <- df$phat_dstar[df$att22==0]/df$phat_dstar_C[df$att22==0]
-	df$sw2[df$att22==1] <- df$phat_d[df$att22==1]/df$phat_d_C[df$att22==1]
-	df$sw3[df$att22==1] <- (df$phat_dstar_CM[df$att22==1]*df$phat_d[df$att22==1])/(df$phat_d_CM[df$att22==1]*df$phat_dstar_C[df$att22==1])
-
-	for (i in c("sw1", "sw2", "sw3")) {
-		df[which(df[,i] > quantile(df[,i], probs=0.99, na.rm=T)), i] <- quantile(df[,i], probs=0.99, na.rm=T)
-		df[which(df[,i] < quantile(df[,i], probs=0.01, na.rm=T)), i] <- quantile(df[,i], probs=0.01, na.rm=T)
-		}
-
-	Ehat_Y0M0 <- weighted.mean(df$std_cesd_age40[df$att22==0], df$sw1[df$att22==0])
-	Ehat_Y1M1 <- weighted.mean(df$std_cesd_age40[df$att22==1], df$sw2[df$att22==1])
-	Ehat_Y1M0 <- weighted.mean(df$std_cesd_age40[df$att22==1], df$sw3[df$att22==1])
-
-	ATE <- Ehat_Y1M1-Ehat_Y0M0
-	NDE <- Ehat_Y1M0-Ehat_Y0M0
-	NIE <- Ehat_Y1M1-Ehat_Y1M0
-
-	point.est <- list(ATE, NDE, NIE)
-
-	return(point.est)
-	}
-
-#setup parallel computing cluster
-my.cluster <- parallel::makeCluster(ncores,type="PSOCK")
-doParallel::registerDoParallel(cl=my.cluster)
-clusterExport(cl=my.cluster, list("ipwmed"), envir=environment())
-registerDoRNG(3308004)
-
-#compute bootstrap estimates
-ipw.boot <- foreach(i=1:nboot, .combine=cbind) %dopar% {
-	boot.data <- nlsy[sample(nrow(nlsy), nrow(nlsy), replace=TRUE),]
-
-	boot.est <- ipwmed(boot.data)
-	
-	return(boot.est)
-	}
-
-stopCluster(my.cluster)
-rm(my.cluster)
-
-ipw.boot <- as.data.frame(matrix(unlist(ipw.boot), ncol=3, byrow=TRUE))
-
-ggplot(ipw.boot, aes(x=V3)) +
-	geom_histogram(binwidth=0.00125, color="black", fill="grey90") +
-	#geom_density(adjust=0.8, alpha=0.1, fill="black") +
-	geom_vline(xintercept=quantile(ipw.boot[,3], prob=0.025), linetype=2, linewidth=0.5) +
-	geom_vline(xintercept=quantile(ipw.boot[,3], prob=0.975), linetype=2, linewidth=0.5) +
- 	theme_bw() +
-	scale_y_continuous(name="Frequency", limits=c(0, 160), breaks=seq(0, 160, 20)) +
-	scale_x_continuous(name=expression(hat(theta)[b]), limits=c(-0.05, 0.02), breaks=round(seq(-0.05, 0.02, 0.01), 2)) 
+# Description: Replicates Chapter 3, Figure 3-7: Histogram of Bootstrap 
+#              Estimates for NIE-hat(1,0)^ipw based on the NLSY.
+#-------------------------------------------------------------------------------
 
 
-ggsave(paste(figdir, "figure_3-7.png", sep=""), height=4.5, width=4.5, units="in", dpi=600)
+#-------------#
+#  LIBRARIES  #
+#-------------#
+library(tidyverse)
+library(haven)
 
+
+
+
+#-----------------------------#
+#  LOAD CAUSAL MED FUNCTIONS  #
+#-----------------------------#
+# utilities
+#source("https://raw.githubusercontent.com/causalMedAnalysis/causalMedR/refs/heads/main/utils.R")
+source("C:/Users/ashiv/OneDrive/Documents/Wodtke/Causal Mediation Analysis Book/Programming/Programs/test project/R/utils_bare.R")
+# IPW estimator
+#source("https://raw.githubusercontent.com/causalMedAnalysis/causalMedR/refs/heads/main/ipwmed.R")
+source("C:/Users/ashiv/OneDrive/Documents/Wodtke/Causal Mediation Analysis Book/Programming/Programs/test project/R/ipwmed.R")
+
+
+
+
+#------------------#
+#  SPECIFICATIONS  #
+#------------------#
+# outcome
+Y <- "std_cesd_age40"
+
+# exposure
+D <- "att22"
+
+# mediator
+M <- "ever_unemp_age3539"
+
+# baseline confounder(s)
+C <- c(
+  "female",
+  "black",
+  "hispan",
+  "paredu",
+  "parprof",
+  "parinc_prank",
+  "famsize",
+  "afqt3"
+)
+
+# key variables
+key_vars <- c(
+  "cesd_age40", # unstandardized version of Y
+  D,
+  M,
+  C
+)
+
+# number of bootstrap replications
+n_reps <- 2000
+
+
+
+
+#----------------#
+#  PREPARE DATA  #
+#----------------#
+nlsy_raw <- read_stata(
+  #file = "https://raw.githubusercontent.com/causalMedAnalysis/repFiles/refs/heads/main/data/NLSY79/nlsy79BK_ed2.dta"
+  file = "C:/Users/ashiv/OneDrive/Documents/Wodtke/Causal Mediation Analysis Book/Programming/Data/NLSY79/nlsy79BK_ed2.dta"
+)
+
+nlsy <- nlsy_raw[complete.cases(nlsy_raw[,key_vars]),] |>
+  mutate(
+    std_cesd_age40 = (cesd_age40 - mean(cesd_age40)) / sd(cesd_age40)
+  )
+
+
+
+
+#----------------------------------------#
+#  ESTIMATE EFFECTS & PERFORM BOOTSTRAP  #
+#----------------------------------------#
+# Additive logit models
+
+# D model 1 formula: f(D|C)
+predictors1_D <- paste(C, collapse = " + ")
+formula1_D_string <- paste(D, "~", predictors1_D)
+formula1_D_string
+
+# D model 2 formula: s(D|C,M)
+predictors2_D <- paste(c(M,C), collapse = " + ")
+formula2_D_string <- paste(D, "~", predictors2_D)
+formula2_D_string
+
+# M model formula: g(M|C,D)
+predictors_M <- paste(c(D,C), collapse = " + ")
+formula_M_string <- paste(M, "~", predictors_M)
+formula_M_string
+
+# Estimate ATE(1,0), NDE(1,0), NIE(1,0)
+out1 <- ipwmed(
+  data = nlsy,
+  D = D,
+  M = M,
+  Y = Y,
+  formula1_string = formula1_D_string,
+  formula2_string = formula2_D_string,
+  boot = TRUE,
+  boot_reps = n_reps,
+  boot_seed = 3308004,
+  boot_parallel = TRUE
+  # ^ Note that parallelizing the bootstrap is optional, but requires that you 
+  # have installed the following R packages: doParallel, doRNG, foreach.
+  # (You do not need to load those packages beforehand, with the library 
+  # function.)
+  # If you choose not to parallelize the bootstrap (by setting the boot_parallel 
+  # argument to FALSE), the results may differ slightly, due to simulation 
+  # variance (even if you specify the same seed).
+)
+
+
+
+
+#-----------------#
+#  CREATE FIGURE  #
+#-----------------#
+data.frame(NIE = out1$boot_NIE) |>
+  ggplot(aes(x=NIE)) +
+  geom_histogram(binwidth=0.00125, color="black", fill="grey90") +
+  #geom_density(adjust=0.8, alpha=0.1, fill="black") +
+  geom_vline(xintercept=out1$ci_NIE[1], linetype=2, linewidth=0.5) +
+  geom_vline(xintercept=out1$ci_NIE[2], linetype=2, linewidth=0.5) +
+  theme_bw() +
+  scale_y_continuous(
+    name = "Frequency",
+    limits = c(0, 160),
+    breaks = seq(0, 160, 20)
+  ) +
+  scale_x_continuous(
+    name = expression(hat(theta)[b]),
+    limits = c(-0.05, 0.02),
+    breaks = round(seq(-0.05, 0.02, 0.01), 2)
+  )
+
+# Save the figure
+ggsave(
+  paste0(dir_fig, "/", title, ".png"),
+  height = 4.5,
+  width = 4.5,
+  units = "in",
+  dpi = 600
+)
+
+
+# Close log
 sink()
+
