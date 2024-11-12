f20dc178062433c6ef7524d7159ea80cacf3b7aa
ashivji1 <ashivji@gmail.com>
2024-11-12 03:20:02 -0600

    Add files via upload

--- code/ch4/table_4-4/table_4-4.R
+++ code/ch4/table_4-4/table_4-4.R
@@ -1,180 +1,448 @@
-###Table 4.4###
+# Preliminaries
+chapter <- "ch4"
+title <- "table_4-4"
+dir_root <- "C:/Users/ashiv/OneDrive/Documents/Wodtke/Causal Mediation Analysis Book/Programming/Programs/Replication"
+dir_log <- paste0(dir_root, "/code/", chapter, "/_LOGS")
+log_path <- paste0(dir_log, "/", title, "_log.txt")
+dir_fig <- paste0(dir_root, "/figures/", chapter)
 
-rm(list=ls())
+# Open log
+sink(log_path, split = TRUE)
+#-------------------------------------------------------------------------------
+# Causal Mediation Analysis Replication Files
 
-packages<-c("dplyr", "tidyr")
+# GitHub Repo: https://github.com/causalMedAnalysis/repFiles/tree/main
 
-#install.packages(packages)
+# Script:      .../code/ch4/table_4-4.R
 
-for (package.i in packages) {
-	suppressPackageStartupMessages(library(package.i, character.only=TRUE))
-	}
+# Inputs:      https://raw.githubusercontent.com/causalMedAnalysis/repFiles/refs/heads/main/data/NLSY79/nlsy79BK_ed2.dta
 
-##office
-datadir <- "C:/Users/Geoffrey Wodtke/Dropbox/shared/causal_mediation_text/data/" 
-logdir <- "C:/Users/Geoffrey Wodtke/Dropbox/shared/causal_mediation_text/code/ch4/_LOGS/"
+# Outputs:     .../code/ch4/_LOGS/table_4-4_log.txt
 
-##home
-#datadir <- "C:/Users/Geoff/Dropbox/shared/causal_mediation_text/data/" 
-#logdir <- "C:/Users/Geoff/Dropbox/shared/causal_mediation_text/code/ch4/_LOGS/"
+# Description: Replicates Chapter 4, Table 4.4: Interventional Effects of 
+#              College Attendance on CES-D Scores as Estimated from the NLSY 
+#              Using Inverse Probability Weighting.
+#-------------------------------------------------------------------------------
 
-##input data 
-nlsy <- as.data.frame(readRDS(paste(datadir, "NLSY79/nlsy79BK_ed2.RDS", sep="")))
 
-nlsy <- nlsy[complete.cases(nlsy[,c("cesd_age40", "ever_unemp_age3539", "faminc_adj_age3539", "log_faminc_adj_age3539", 
-	"att22", "female", "black", "hispan", "paredu", "parprof", "parinc_prank", "famsize", "afqt3")]),]
+#-------------#
+#  LIBRARIES  #
+#-------------#
+library(tidyverse)
+library(haven)
 
-nlsy$std_cesd_age40 <- (nlsy$cesd_age40-mean(nlsy$cesd_age40))/sd(nlsy$cesd_age40)
 
-##weighting estimators
 
-#define estimation function 
-ipwmed <- function(data, Dform, Lform, Mform) {
 
-	df <- data
+#-----------------------------#
+#  LOAD CAUSAL MED FUNCTIONS  #
+#-----------------------------#
+# utilities
+source("https://raw.githubusercontent.com/causalMedAnalysis/causalMedR/refs/heads/main/utils.R")
 
-	Dmodel <- glm(Dform, data=df, family=binomial("logit"))
 
-	Lmodel <- glm(Lform, data=df, family=binomial("logit"))
+
+
+#------------------#
+#  SPECIFICATIONS  #
+#------------------#
+# outcome
+Y <- "std_cesd_age40"
+
+# exposure
+D <- "att22"
+
+# mediator
+M <- "log_faminc_adj_age3539"
+
+# exposure-induced confounder
+L <- "ever_unemp_age3539"
+
+# baseline confounder(s)
+C <- c(
+  "female",
+  "black",
+  "hispan",
+  "paredu",
+  "parprof",
+  "parinc_prank",
+  "famsize",
+  "afqt3"
+)
+
+# key variables
+key_vars <- c(
+  "cesd_age40", # unstandardized version of Y
+  D,
+  M,
+  L,
+  C
+)
+
+# mediator value for CDE
+m <- log(5e4)
+
+
+
+
+#----------------#
+#  PREPARE DATA  #
+#----------------#
+nlsy_raw <- read_stata(
+  file = "https://raw.githubusercontent.com/causalMedAnalysis/repFiles/refs/heads/main/data/NLSY79/nlsy79BK_ed2.dta"
+)
+
+nlsy <- nlsy_raw[complete.cases(nlsy_raw[,key_vars]),] |>
+  mutate(
+    std_cesd_age40 = (cesd_age40 - mean(cesd_age40)) / sd(cesd_age40)
+  )
+
+
+
+
+#------------------------------#
+#  DEFINE CUSTOM IPW FUNCTION  #
+#------------------------------#
+custom_ipwvent <- function(
+  data,
+  D,
+  M,
+  Y,
+  L,
+  m = 0,
+  D_formula,
+  L_formula,
+  M_formula,
+  stabilize = TRUE,
+  censor = TRUE,
+  censor_low = 0.01,
+  censor_high = 0.99
+) {
+  # fit specified models
+  ## estimate f(D|C)
+  D_model <- glm(
+    D_formula,
+    data = data,
+    family = binomial(link = "logit")
+  )
+  ## estimate q(L|C,D)
+  L_model <- glm(
+    L_formula,
+    data = data,
+    family = binomial(link = "logit")
+  )
+  ## estimate g(M|C,D,L)
+  M_model <- lm(
+    M_formula,
+    data = data
+  )
   
-	Mmodel <- lm(Mform, data=df)
+  # D and L values
+  D_values <- unique(data[[D]])
+  L_values <- unique(data[[L]])
+  cross_DL <- expand.grid(D_val = D_values, L_val = L_values)
+  
+  # estimate p(D|C) for each value of D
+  pDd_C <- lapply(
+    D_values,
+    \(d) dbinom(
+      d,
+      size = 1,
+      prob = predict(D_model, newdata = data, type = "response")
+    )
+  )
+  names(pDd_C) <- paste0("pD", D_values, "_C")
+  list2env(pDd_C, envir = environment())
+  
+  # estimate p(D|C) for the observed D
+  pD_C <- dbinom(
+    data[[D]],
+    size = 1,
+    prob = predict(D_model, newdata = data, type = "response")
+  )
+  
+  # estimate p(D) for each value of D
+  pD1 <- mean(data[[D]])
+  pD0 <- 1 - pD1
+  
+  # estimate p(D) for the observed D
+  pD <- ifelse(as.logical(data[[D]]), pD1, pD0)
+  
+  # estimate p(L|D,C) for each value of L and D
+  pLl_DdC <- mapply(
+    function(d, l) {
+      temp_data <- data
+      temp_data[[D]] <- d
+      dbinom(
+        l,
+        size = 1,
+        prob = predict(L_model, newdata = temp_data, type = "response")
+      )
+    },
+    cross_DL$D_val,
+    cross_DL$L_val,
+    SIMPLIFY = FALSE
+  )
+  names(pLl_DdC) <- paste0("pL", cross_DL$L_val, "_D", cross_DL$D_val, "C")
+  list2env(pLl_DdC, envir = environment())
+  
+  # estimate p(M|D,L,C) for the observed M and each value of D and L
+  pM_DdLlC <- mapply(
+    function(d, l) {
+      temp_data <- data
+      temp_data[[D]] <- d
+      temp_data[[L]] <- l
+      dnorm(
+        temp_data[[M]],
+        mean = predict(M_model, newdata = temp_data, type = "response"),
+        sd = sigma(M_model)
+      )
+    },
+    cross_DL$D_val,
+    cross_DL$L_val,
+    SIMPLIFY = FALSE
+  )
+  names(pM_DdLlC) <- paste0("pM_D", cross_DL$D_val, "L", cross_DL$L_val, "C")
+  list2env(pM_DdLlC, envir = environment())
+  
+  # estimate p(M|D,L,C) for the observed M, observed L, and each value of D
+  pM_DdLC <- lapply(
+    D_values,
+    function(d) {
+      temp_data <- data
+      temp_data[[D]] <- d
+      dnorm(
+        temp_data[[M]],
+        mean = predict(M_model, newdata = temp_data, type = "response"),
+        sd = sigma(M_model)
+      )
+    }
+  )
+  names(pM_DdLC) <- paste0("pM_D", D_values, "LC")
+  list2env(pM_DdLC, envir = environment())
+  
+  # estimate p(M|D,L,C) for the observed M, observed D, and observed L
+  pM_DLC <- dnorm(
+    data[[M]],
+    mean = predict(M_model, newdata = data, type = "response"),
+    sd = sigma(M_model)
+  )
+  
+  # estimate p(M|D) for the observed M and observed D
+  M_mean <- ave(data[[M]], data[[D]])
+  M_dev <- data[[M]] - M_mean
+  pM_D <- dnorm(
+    data[[M]],
+    mean = M_mean,
+    sd = sd(M_dev)
+  )
+  
+  # for convenience, create logical vectors to identify subgroups
+  group_D0 <- data[[D]]==0
+  group_D1 <- data[[D]]==1
+  
+  # create IPWs
+  w1 <- ifelse(
+    group_D0,
+    ((pM_D0L0C * pL0_D0C) + (pM_D0L1C * pL1_D0C)) /
+      (pD0_C * pM_D0LC)
+    ,
+    0
+  )
+  w2 <- ifelse(
+    group_D1,
+    ((pM_D1L0C * pL0_D1C) + (pM_D1L1C * pL1_D1C)) /
+      (pD1_C * pM_D1LC)
+    ,
+    0
+  )
+  w3 <- ifelse(
+    group_D1,
+    ((pM_D0L0C * pL0_D0C) + (pM_D0L1C * pL1_D0C)) /
+      (pD1_C * pM_D1LC)
+    ,
+    0
+  )
+  w4 <- 1 / (pM_DLC * pD_C)
+  
+  # stabilize IPWs
+  if (stabilize) {
+    w1 <- w1 * pD0
+    w2 <- w2 * pD1
+    w3 <- w3 * pD1
+    w4 <- w4 * pM_D * pD
+  }
+  
+  # censor IPWs (among the appropriate subgroups with non-zero IPWs)
+  if (censor) {
+    w1[group_D0] <- trimQ(w1[group_D0], low = censor_low, high = censor_high)
+    w2[group_D1] <- trimQ(w2[group_D1], low = censor_low, high = censor_high)
+    w3[group_D1] <- trimQ(w3[group_D1], low = censor_low, high = censor_high)
+    w4 <- trimQ(w4, low = censor_low, high = censor_high)
+  }
+  
+  # estimate OE, IDE, and IIE
+  Ehat_Y0M0 <- weighted.mean(data[[Y]], w1)
+  Ehat_Y1M1 <- weighted.mean(data[[Y]], w2)
+  Ehat_Y1M0 <- weighted.mean(data[[Y]], w3)
+  OE  <- Ehat_Y1M1 - Ehat_Y0M0
+  IDE <- Ehat_Y1M0 - Ehat_Y0M0
+  IIE <- Ehat_Y1M1 - Ehat_Y1M0
+  
+  # estimate CDE
+  Y_model <- lm(
+    as.formula(paste0(Y,"~",D,"*",M)),
+    data = data,
+    weights = w4
+  )
+  CDE <- 
+    Y_model$coefficients[[D]] + 
+    Y_model$coefficients[[paste0(D,":",M)]] * m
+  
+  # compile and output
+  out <- list(
+    OE = OE,
+    IDE = IDE,
+    IIE = IIE,
+    CDE = CDE,
+    weights1 = w1,
+    weights2 = w2,
+    weights3 = w3,
+    weights4 = w4,
+    model_D = D_model,
+    model_L = L_model,
+    model_M = M_model
+  )
+  return(out)
+}
 
-	idataD0 <- df %>% mutate(att22 = 0)
-	idataD1 <- df %>% mutate(att22 = 1)
 
-	idataD0L0 <- df %>% mutate(att22 = 0, ever_unemp_age3539 = 0)
-	idataD1L0 <- df %>% mutate(att22 = 1, ever_unemp_age3539 = 0)
-	idataD0L1 <- df %>% mutate(att22 = 0, ever_unemp_age3539 = 1)
-	idataD1L1 <- df %>% mutate(att22 = 1, ever_unemp_age3539 = 1)
 
-	phatD_C <- df %>% 
-		mutate(
-			pD1_C = predict(Dmodel, newdata=df, type = "response"),
-			pD0_C = 1-pD1_C,
-			pD1 = mean(att22),
-			pD0 = 1-pD1) %>%
-		select(id, pD1_C, pD0_C, pD1, pD0)
 
-	phatL_D1C <- idataD1 %>% 
-		mutate(
-			pL1_D1C = predict(Lmodel, newdata=idataD1, type = "response"),
-			pL0_D1C = 1-pL1_D1C) %>%
-		select(id, pL1_D1C, pL0_D1C)
+#-------------#
+#  VERSION 1  #
+#-------------#
+# D model: Additive logit model
+# L model: Additive logit model
+# M model: Additive linear model
 
-	phatL_D0C <- idataD0 %>% 
-		mutate(
-			pL1_D0C = predict(Lmodel, newdata=idataD0, type = "response"),
-			pL0_D0C = 1-pL1_D0C) %>%
-		select(id, pL1_D0C, pL0_D0C)
+# D model formula
+predictors1_D <- paste(C, collapse = " + ")
+(formula1_D_string <- paste(D, "~", predictors1_D))
+formula1_D <- as.formula(formula1_D_string)
 
-	phatM_D <- df %>%
-		mutate(
-			pM_D1 = case_when(att22 == 1 ~ dnorm(log_faminc_adj_age3539, mean(df$log_faminc_adj_age3539[df$att==1]), sd=sd(df$log_faminc_adj_age3539-(mean(df$log_faminc_adj_age3539[df$att==1])*df$att22)-(mean(df$log_faminc_adj_age3539[df$att==0])*(1-df$att22))))),
-			pM_D0 = case_when(att22 == 0 ~ dnorm(log_faminc_adj_age3539, mean(df$log_faminc_adj_age3539[df$att==0]), sd=sd(df$log_faminc_adj_age3539-(mean(df$log_faminc_adj_age3539[df$att==1])*df$att22)-(mean(df$log_faminc_adj_age3539[df$att==0])*(1-df$att22)))))) %>%
-		select(id, pM_D1, pM_D0) 
+# L model formula
+predictors1_L <- paste(c(D,C), collapse = " + ")
+(formula1_L_string <- paste(L, "~", predictors1_L))
+formula1_L <- as.formula(formula1_L_string)
 
-	phatM_D1LC <- idataD1 %>% 
-		mutate(
-			pM_D1LC = dnorm(log_faminc_adj_age3539, predict(Mmodel, newdata=idataD1, type = "response"), sd=sigma(Mmodel))) %>%
-		select(id, pM_D1LC)
+# M model formula
+predictors1_M <- paste(c(D,C,L), collapse = " + ")
+(formula1_M_string <- paste(M, "~", predictors1_M))
+formula1_M <- as.formula(formula1_M_string)
 
-	phatM_D0LC <- idataD1 %>% 
-		mutate(pM_D0LC = dnorm(log_faminc_adj_age3539, predict(Mmodel, newdata=idataD0, type = "response"), sd=sigma(Mmodel))) %>%
-		select(id, pM_D0LC)
+# Estimate effects
+out1 <- custom_ipwvent(
+  data = nlsy,
+  D = D,
+  M = M,
+  Y = Y,
+  L = L,
+  m = m,
+  D_formula = formula1_D,
+  L_formula = formula1_L,
+  M_formula = formula1_M
+)
 
-	phatM_D0L0C <- idataD0L0 %>% 
-		mutate(pM_D0L0C = dnorm(log_faminc_adj_age3539, predict(Mmodel, newdata=idataD0L0, type = "response"), sd=sigma(Mmodel))) %>%
-		select(id, pM_D0L0C)
 
-	phatM_D1L0C <- idataD1L0 %>% 
-		mutate(pM_D1L0C = dnorm(log_faminc_adj_age3539, predict(Mmodel, newdata=idataD1L0, type = "response"), sd=sigma(Mmodel))) %>%
-		select(id, pM_D1L0C)
 
-	phatM_D0L1C <- idataD0L1 %>% 
-		mutate(pM_D0L1C = dnorm(log_faminc_adj_age3539, predict(Mmodel, newdata=idataD0L1, type = "response"), sd=sigma(Mmodel))) %>%
-		select(id, pM_D0L1C)
 
-	phatM_D1L1C <- idataD1L1 %>% 
-		mutate(pM_D1L1C = dnorm(log_faminc_adj_age3539, predict(Mmodel, newdata=idataD1L1, type = "response"), sd=sigma(Mmodel))) %>%
-		select(id, pM_D1L1C)
+#-------------#
+#  VERSION 2  #
+#-------------#
+# D model: Additive logit model
+# L model: Logit model with D x C interactions
+# M model: Linear model with D x C, D x L interactions
 
-	df.wts <- df %>%
-	    full_join(phatD_C, by = "id") %>%
-	    full_join(phatL_D1C, by = "id") %>%
-	    full_join(phatL_D0C, by = "id") %>%
-	    full_join(phatM_D, by = "id") %>%
-	    full_join(phatM_D1LC, by = "id") %>%
-	    full_join(phatM_D0LC, by = "id") %>%
-	    full_join(phatM_D0L0C, by = "id") %>%
-	    full_join(phatM_D1L0C, by = "id") %>%
-	    full_join(phatM_D0L1C, by = "id") %>%
-	    full_join(phatM_D1L1C, by = "id")
+# D model formula
+formula2_D <- formula1_D
 
-	df.wts <- df.wts %>%
-		mutate(
-			sw1 = case_when(att22 == 0 ~ (pD0/pD0_C) * (1/pM_D0LC) * ((pM_D0L0C * pL0_D0C) + (pM_D0L1C * pL1_D0C))),
-			sw2 = case_when(att22 == 1 ~ (pD1/pD1_C) * (1/pM_D1LC) * ((pM_D1L0C * pL0_D1C) + (pM_D1L1C * pL1_D1C))),
-			sw3 = case_when(att22 == 1 ~ (pD1/pD1_C) * (1/pM_D1LC) * ((pM_D0L0C * pL0_D0C) + (pM_D0L1C * pL1_D0C))),
-			sw4 = case_when(
-				att22 == 1 ~ (pD1/pD1_C) * (pM_D1/pM_D1LC),
-				att22 == 0 ~ (pD0/pD0_C) * (pM_D0/pM_D0LC))) %>%
-		mutate(
-			across(c(sw1, sw2, sw3, sw4), 
-			~ifelse(. < quantile(., 0.01, na.rm = TRUE), quantile(., 0.01, na.rm = TRUE),
-                   ifelse(. > quantile(., 0.99, na.rm = TRUE), quantile(., 0.99, na.rm = TRUE), .))))
+# L model formula
+## main effects
+predictors2_L <- paste(c(D,C), collapse = " + ")
+## D x C interactions
+predictors2_L <- paste(
+  predictors2_L,
+  "+",
+  paste(D, C, sep = ":", collapse = " + ")
+)
+## full formula
+(formula2_L_string <- paste(L, "~", predictors2_L))
+formula2_L <- as.formula(formula2_L_string)
 
-	Ehat_Y0M0 <- weighted.mean(df.wts$std_cesd_age40[df.wts$att22==0], df.wts$sw1[df$att22==0])
-	Ehat_Y1M1 <- weighted.mean(df.wts$std_cesd_age40[df.wts$att22==1], df.wts$sw2[df$att22==1])
-	Ehat_Y1M0 <- weighted.mean(df.wts$std_cesd_age40[df.wts$att22==1], df.wts$sw3[df$att22==1])
+# M model formula
+## main effects
+predictors2_M <- paste(c(D,C,L), collapse = " + ")
+## D x C interactions
+predictors2_M <- paste(
+  predictors2_M,
+  "+",
+  paste(D, C, sep = ":", collapse = " + ")
+)
+## D x L interaction
+predictors2_M <- paste(
+  predictors2_M,
+  "+",
+  paste(D, L, sep = ":", collapse = " + ")
+)
+## full formula
+(formula2_M_string <- paste(M, "~", predictors2_M))
+formula2_M <- as.formula(formula2_M_string)
 
-	OE <- Ehat_Y1M1-Ehat_Y0M0
-	IDE <- Ehat_Y1M0-Ehat_Y0M0
-	IIE <- Ehat_Y1M1-Ehat_Y1M0
+# Estimate effects
+out2 <- custom_ipwvent(
+  data = nlsy,
+  D = D,
+  M = M,
+  Y = Y,
+  L = L,
+  m = m,
+  D_formula = formula2_D,
+  L_formula = formula2_L,
+  M_formula = formula2_M
+)
 
-	Ymodel.wtd <- lm(std_cesd_age40 ~ att22 * log_faminc_adj_age3539, data=df.wts, weights=sw4)
 
-	CDE <- Ymodel.wtd$coefficients["att22"] + log(50000)*Ymodel.wtd$coefficients["att22:log_faminc_adj_age3539"]
 
-	##### NEED TO ADD CDE ESTIMATES #####
 
-	point.est <- list(OE, IDE, IIE, CDE)
+#---------------------#
+#  COLLATE ESTIMATES  #
+#---------------------#
+master <- data.frame(
+  param = c("OE(1,0)", "IDE(1,0)", "IIE(1,0)", "CDE(1,0,ln(50K))"),
+  est_v1 = c(
+    out1$OE,
+    out1$IDE,
+    out1$IIE,
+    out1$CDE
+  ),
+  est_v2 = c(
+    out2$OE,
+    out2$IDE,
+    out2$IIE,
+    out2$CDE
+  )
+)
 
-	return(point.est)
-	}
+master |>
+  mutate(
+    across(
+      .cols = starts_with("est_"),
+      .fns = \(x) round(x, 3)
+    )
+  )
 
-#compute ipw estimates w/ additive models
-Dform.a <- att22 ~ female + black + hispan + paredu + parprof + parinc_prank + famsize + afqt3
 
-Lform.a <- ever_unemp_age3539 ~ att22 + female + black + hispan + paredu + parprof + parinc_prank + famsize + afqt3
-
-Mform.a <- log_faminc_adj_age3539 ~ att22 + ever_unemp_age3539 + female + black + hispan + paredu + parprof + parinc_prank + famsize + afqt3
-
-ipwmed.est.a <- ipwmed(data=nlsy, Dform=Dform.a, Lform=Lform.a, Mform=Mform.a)
-
-#compute ipw estimates w/ interactive models
-Dform.x <- att22 ~ female + black + hispan + paredu + parprof + parinc_prank + famsize + afqt3
-
-Lform.x <- ever_unemp_age3539 ~ att22 * (female + black + hispan + paredu + parprof + parinc_prank + famsize + afqt3)
-
-Mform.x <- log_faminc_adj_age3539 ~ att22 * (ever_unemp_age3539 + female + black + hispan + paredu + parprof + parinc_prank + famsize + afqt3)
-
-ipwmed.est.x <- ipwmed(data=nlsy, Dform=Dform.x, Lform=Lform.x, Mform=Mform.x)
-
-#print output
-ipwmed.est.a <- matrix(unlist(ipwmed.est.a), ncol=4, byrow=TRUE)
-ipwmed.est.x <- matrix(unlist(ipwmed.est.x), ncol=4, byrow=TRUE)
-
-ipwmed.a.out <- data.frame(est = round(c(ipwmed.est.a), digits=3), row.names=c("OEhat^ipw", "IDEhat^ipw", "IIEhat^ipw", "CDEhat^ipw"))
-ipwmed.x.out <- data.frame(est = round(c(ipwmed.est.x), digits=3), row.names=c("OEhat^ipw", "IDEhat^ipw", "IIEhat^ipw", "CDEhat^ipw"))
-
-sink(paste(logdir, "table_4-4_log.txt", sep=""))
-
-cat("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-cat("ipwEst w/ additive models for D, L, and ln(M)\n")
-print(ipwmed.a.out)
-cat("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-cat("ipwEst w/ interactive models for L and ln(M)\n")
-print(ipwmed.x.out)
-cat("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-
+# Close log
 sink()
+
