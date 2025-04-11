31bbcd5d068369d222547829d68af09398d94264
vigiwang <weiqiw@uchicago.com>
2025-04-11 11:38:57 -0500

    Clean figure_5-6.R

--- code/ch5/figure_5-6/figure_5-6.R
+++ code/ch5/figure_5-6/figure_5-6.R
@@ -1,147 +1,314 @@
-###Figure 5.6###
-
+#----------Preliminaries---------#
 rm(list=ls())
+chapter <- "ch5"
+title <- "figure_5-6"
 
-packages<-c("tidyverse", "margins", "mediation", "foreach", "doParallel", "doRNG")
+# Specify the root directory:
+dir_root <- "Please Change to Your Local Directory" 
 
-for (package.i in packages) {
-  suppressPackageStartupMessages(library(package.i, character.only=TRUE))
+# Define subdirectories for logs and figures:
+dir_log <- paste0(dir_root, "/code/", chapter, "/_LOGS")
+log_path <- paste0(dir_log, "/", title, "_log.txt")
+dir_fig <- paste0(dir_root, "/figures/", chapter)
+dir_tab <- paste0(dir_root, "/table/", chapter)
+
+# Ensure all necessary directories exist under your root folder，
+# if not, the function will create folders for you:
+
+create_dir_if_missing <- function(dir) {
+  if (!dir.exists(dir)) {
+    dir.create(dir, recursive = TRUE)
+    message("Created directory: ", dir)
+  } else {
+    message("Directory already exists: ", dir)
+  }
 }
-source("utils.R")
 
-# devtools::install_github("xiangzhou09/paths")
-library(paths)
-library(latex2exp)
+create_dir_if_missing(dir_root)
+create_dir_if_missing(dir_log)
+create_dir_if_missing(dir_fig)
+create_dir_if_missing(dir_tab)
 
-##office
-datadir <- "../../data/" 
-logdir <- "../../code/ch5/_LOGS/"
+# Open log
+sink(log_path, split = TRUE)
 
-##input data
-load("Brader_et_al2008.RData")
+#-------------------------------------------------------------------------------
+# Causal Mediation Analysis Replication Files
 
-# function for demeaning
-demean <- function(x) x - mean(x, na.rm = TRUE)
+# GitHub Repo: https://github.com/causalMedAnalysis/repFiles/tree/main
 
-# data preprocessing
-Brader2 <- Brader %>%
-  dplyr::select(immigr, emo, p_harm, tone_eth, ppage, ppeducat, ppgender, ppincimp) %>% na.omit() %>%
-  mutate(immigr = as.numeric(scale(4 - immigr)),
-         hs = (ppeducat == "high school"),
-         sc = (ppeducat == "some college"),
-         ba = (ppeducat == "bachelor's degree or higher"),
-         female = (ppgender == "female")) %>%
-  mutate_at(vars(emo, p_harm, ppage, female, hs, sc, ba, ppincimp), demean)
+# Script:      .../code/ch5/table_5-9.R
 
-summary(Brader2)
+# Inputs:      https://raw.githubusercontent.com/causalMedAnalysis/causalMedR/refs/heads/main/utils.R
 
-d <- "tone_eth"
-m1 <- "p_harm"
-m2 <- "emo"
-y <- "immigr"
-x <- c("ppage", "female", "hs", "sc", "ba", "ppincimp")
 
-df <- Brader2
-m12 <- c(m1, m2)
+# Outputs:     .../code/ch5/_LOGS/figure_5-6_log.txt
 
-set.seed(02138)
+#              .../figures/ch5/figure_5-6.png
 
-##########################################
-## RI without interactions
-##########################################
 
-mediators <- list(m1, m2)
 
-formula_m0 <- as.formula(paste(y, "~", paste(c(x, d), collapse = " + ")))
-formula_m1 <- as.formula(paste(y, "~", paste(c(x, d, m1), collapse = " + ")))
-formula_m2 <- as.formula(paste(y, "~", paste(c(x, d, m1, m2), collapse = " + ")))
+# Description: Replicates Chapter 5, Figure 5.6: Conditional Difference in the 
+#                                 the Prevalence of U across levels of D(δ DU)
 
-# outcome models
-glm_m0 <- glm(formula_m0, data = df)
-glm_m1 <- glm(formula_m1, data = df)
-glm_m2 <- glm(formula_m2, data = df)
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------#
+#  INSTALL DEPENDENCIES and LOAD RERUIRED PACKAGES
+#------------------------------------------------#
+
+# The following packages are required for replicate results:
+packages <-
+  c(
+    "tidyverse", 
+    "paths",
+    "latex2exp"
+  )
+
+# Below function will automatically download the package you need,
+# otherwise simply load the package:
+install_and_load <- function(pkg_list) {
+  for (pkg in pkg_list) {
+    if (!requireNamespace(pkg, quietly = TRUE)) {  
+      message("Installing missing package: ", pkg)
+      install.packages(pkg, dependencies = TRUE)  
+    }
+    library(pkg, character.only = TRUE)  
+  }
+}
+
+install_and_load(packages)
+
+#-----------------------------#
+#  LOAD CAUSAL MED FUNCTIONS  #
+#-----------------------------#
+
+# helper functions:
+source("https://raw.githubusercontent.com/causalMedAnalysis/causalMedR/refs/heads/main/utils.R")
+source("https://raw.githubusercontent.com/causalMedAnalysis/causalMedR/refs/heads/main/pathimp.R")
+
+
+#------------------#
+#  SPECIFICATIONS  #
+#------------------#
+# outcome
+Y <- "immigr"
+
+# exposure
+D <- "tone_eth"
+
+# mediators
+M1 <- "p_harm"
+M2 <- "emo"
+M <- 
+  list(
+    M1,
+    M2
+  )
+
+# baseline confounder(s)
+C <- c(
+  "ppage", 
+  "female", 
+  "hs", 
+  "sc", 
+  "ba", 
+  "ppincimp",
+  "female_sens",
+  "ba_sens"
+)
+
+# key variables
+key_vars <- c(
+  "immigr", # unstandardized version of Y
+  D,
+  unlist(M),
+  C
+)
+
+# number of bootstrap replications
+nboot <- 2000
+
+# set seed:
+boot_seed <- 02138
+
+#-----------------------------#
+#        PREPARE DATA         #
+#-----------------------------#
+
+# Load the data:
+
+temp_file <- tempfile() # define a placeholder to store the data
+
+download.file(
+  "https://raw.githubusercontent.com/causalMedAnalysis/repFiles/refs/heads/main/data/Brader_et_al2008/Brader_et_al2008.RData", 
+  temp_file, 
+  mode = "wb")
+
+load(temp_file)
+
+# Process the data:
+Brader <- 
+  Brader %>%
+  dplyr::select(
+    immigr, 
+    emo, 
+    p_harm, 
+    tone_eth, 
+    ppage, 
+    ppeducat, 
+    ppgender, 
+    ppincimp) %>% 
+  na.omit() %>%
+  mutate(
+    immigr = as.numeric(scale(4 - immigr)),
+    hs = (ppeducat == "high school"),
+    sc = (ppeducat == "some college"),
+    ba = (ppeducat == "bachelor's degree or higher"),
+    female = (ppgender == "female")) %>%
+  mutate_at(
+    vars(emo, p_harm, ppage, female, hs, sc, ba, ppincimp), 
+    ~ . - mean(., na.rm = TRUE)  
+  ) %>%
+  mutate(
+    female_sens = as.double(ppgender == "female"),
+    ba_sens = as.double(ppeducat == "bachelor's degree or higher")
+  )
+
+
+#------------------------------------------------------------------------------#
+#                            REPLICATE FIGURE 5.6                              #
+#------------------------------------------------------------------------------#
+
+#-----------------------------------------------------------------------------#
+#  Step 1: Fit the Original Imputation Without Interaction Regression        #
+#---------------------------------------------------------------------------#
+
+# Specify outcome models:
+# E(Y|D,C):
+glm_m0 <- glm(
+  immigr ~ ppage + female + hs + sc + ba + ppincimp + tone_eth, 
+  data = Brader
+)
+
+# E(Y|D,C,M1)
+glm_m1 <- glm(
+  immigr ~ ppage + female + hs + sc + ba + ppincimp + tone_eth + p_harm, 
+  data = Brader
+)
+
+# E(Y|D,C,M1,M2)
+glm_m2 <- glm(
+  immigr ~ ppage + female + hs + sc + ba + ppincimp + tone_eth + p_harm + emo, 
+  data = Brader
+)
+
+# Combine all models into a list
 glm_ymodels <- list(glm_m0, glm_m1, glm_m2)
 
-paths_glm <- paths(a = d, y = y, m = mediators,
-                   glm_ymodels, data = df, nboot = 2000)
-
-pure <- paths_glm$pure %>% 
-  filter(decomposition == "Type I") %>% 
-  mutate(estimand = factor(estimand, levels = c("total", "direct", "via M2", "via M1"),
-                           labels = c("ATE", "AY", "AM2Y", "AM1Y"))) %>% 
-  rename(est = estimate) %>% 
-  dplyr::select(-decomposition, -se, -p) %>% 
-  mutate_at(c("est", "lower", "upper"),  ~ round(.x, digits = 3)) %>% 
-  mutate(intv = paste0("(", lower, ", ", upper, ")"))
-  
-  
-hybrid <- paths_glm$hybrid %>% 
-  filter(decomposition == "Type I") %>% 
-  mutate(estimand = factor(estimand, levels = c("total", "direct", "via M2", "via M1"),
-                           labels = c("ATE", "AY", "AM2Y", "AM1Y"))) %>% 
-  rename(est = estimate) %>% 
-  dplyr::select(-decomposition, -se, -p) %>% 
-  mutate_at(c("est", "lower", "upper"),  ~ round(.x, digits = 3)) %>% 
-  mutate(intv = paste0("(", lower, ", ", upper, ")"))
-
-m_out2a <- bind_rows(pure, hybrid) %>% 
-  mutate(estimator = factor(estimator, levels = c("pure", "hybrid"))) %>% 
-  arrange(estimator, estimand) %>% 
-  mutate(out = paste(est, intv)) %>% 
-  add_column(interactions = "n")
+# Fit the Paths Model:
+Paths_Model <-
+  pathimp(
+    D = D,
+    Y = Y,
+    M = M,
+    Y_models = glm_ymodels,
+    D_model = NULL,
+    data = Brader,
+    boot_reps = 2000,
+    boot_seed = boot_seed,
+    out_ipw = FALSE
+  )
 
 
-##########################################
-## Sensitivity Analysis
-##########################################
+#-----------------------------------------------------------------------------#
+#                        Step 2:   Sensitivity Analysis                      #
+#---------------------------------------------------------------------------#
 
-paths_sens <- sens(paths_glm, confounded = "M1", estimand = "direct",
-                   gamma_values = seq(0, 2, 0.005),
-                   eta_values = seq(-0.5, 0.5, 0.005))
+Paths_Sens_Model <- sens(
+  Paths_Model$org_obj, 
+  confounded = "M1", 
+  estimand = "direct",
+  gamma_values = seq(0, 2, 0.005),
+  eta_values = seq(-0.5, 0.5, 0.005)
+)
 
-# gamma2 and eta2 for selected covariates
 
-summary(Brader)
 
-Brader2_ed <- Brader2 %>%
-  mutate(female2 = as.double(ppgender == "female"),
-         ba2 = as.double(ppeducat == "bachelor's degree or higher"))
+#-----------------------------------------------------------------------------#
+#                     Step 3:   Adding Two Reference Points                  #
+#---------------------------------------------------------------------------#
 
-# formulas
-d <- "tone_eth"
-m1 <- "p_harm"
-m2 <- "emo"
-y <- "immigr"
-x <- c("ppage", "female2", "hs", "sc", "ba2", "ppincimp")
+# Model_UY: E(Y | C, D, M1, M2, U)
+# Captures the effect of the confounders on the outcome (Y)
+Model_UY <- lm(
+  immigr ~ ppage + female_sens + hs + sc + 
+    ba_sens + ppincimp + tone_eth + p_harm + emo, 
+  data = Brader
+)
 
-form_y <- as.formula(paste0(y, "~", paste0(c(x, d, m1, m2), collapse = "+")))
-form_female2 <- as.formula(paste0("female2 ~", paste0(c(setdiff(x, "female"), d, m1, m2),
-                                                    collapse = "+")))
-form_ba2 <- as.formula(paste0("ba2 ~", paste0(c(setdiff(x, "ba"), d, m1, m2),
-                                                  collapse = "+"))) 
+# Model_UD_fem: E(U | D, M1, M2, U1)
+# Captures the effect of the treatment (D) on the confounder female_sens
+Model_UD_fem <- lm(
+  female_sens ~ ppage + hs + sc + ba_sens + 
+    ppincimp + tone_eth + p_harm + emo, 
+  data = Brader
+)
 
-mod_y <- lm(form_y, data = Brader2_ed)
-mod_female2 <- lm(form_female2, Brader2_ed)
-mod_ba2 <- lm(form_ba2, Brader2_ed)
+# Model_UD_ba: E(U | D, M1, M2, U2)
+# Captures the effect of the treatment (D) on the confounder ba_sens
+Model_UD_ba <- lm(
+  ba_sens ~ ppage + hs + sc + female_sens + 
+    ppincimp + tone_eth + p_harm + emo, 
+  data = Brader
+)
 
-gamma2_female2 <- mod_y$coefficients["female2"]
-gamma2_ba2 <- mod_y$coefficients["ba2"]
-eta2_female2 <- mod_female2$coefficients[d]
-eta2_ba2 <- mod_ba2$coefficients[d]
 
-xlab_new <- bquote("Conditional Difference in the Prevalence of U across levels of D"~(delta[DU]))
-ylab_new <- bquote("Conditional Difference in the Mean of Y across levels of U"~(delta[UY]))
+# Assume the Counfounder is gender:
+
+delta_UY_fem <- Model_UY$coefficients["female_sens"] # effect of U on Y
+delta_DU_fem <- Model_UD_fem$coefficients["tone_eth"] # effect of U on D
+
+# Assume the Counfounder is Attainment of College Degree:
+
+delta_UY_ba <- Model_UY$coefficients["ba_sens"] # effect of U on Y
+delta_DU_ba <- Model_UD_ba$coefficients["tone_eth"] # effect of U on D
+
+
+#-----------------------------------------------------------------------------#
+#                     Step 4:   Generate the Final Plot                      #
+#---------------------------------------------------------------------------#
 
 
 # sensitivity analysis plot
-plot(paths_sens, outcome_name = "Support for Immigration") +
-  xlab(xlab_new) +
-  ylab(ylab_new) +
-  annotate("point", x = eta2_female2, y = gamma2_female2) +
-  annotate("text", x = eta2_female2 + 0.05, y = gamma2_female2, label = "female", size = 5) +
-  annotate("point", x = eta2_ba2, y = gamma2_ba2) +
-  annotate("text", x = eta2_ba2 - 0.05, y = gamma2_ba2, label = "college\n graduate", size = 5) +
+plot(Paths_Sens_Model, 
+     outcome_name = "Support for Immigration") +
+  xlab(
+    bquote(
+      "Conditional Difference in the Prevalence of U across levels of D"~(delta[DU])
+    )
+  ) +
+  ylab(
+    bquote(
+      "Conditional Difference in the Mean of Y across levels of U"~(delta[UY])
+    )
+  ) +
+  annotate("point", x = delta_DU_fem, y = delta_UY_fem) +
+  annotate("text", x = delta_DU_fem + 0.05, y = delta_UY_fem, label = "female", size = 5) +
+  annotate("point", x = delta_DU_ba, y = delta_UY_ba) +
+  annotate("text", x = delta_DU_ba - 0.05, y = delta_UY_ba, label = "college\n graduate", size = 5) +
   theme_minimal(base_size = 16) +
   scale_fill_manual(values = c(NA, "grey70"),  na.value = NA)
 
-ggsave("figure_5-6.png", width = 10, height = 7)
+#------------------#
+#    SAVE PLOT     #
+#------------------#
+
+ggsave(paste0(dir_fig,"/figure_5-6.png"), width = 10, height = 7)
+
+
+
+
+
+
