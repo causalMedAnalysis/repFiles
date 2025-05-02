174d62e5f3524ab4d7385fe0d48633b928973789
GTW <causalmed@gmail.com>
2025-05-02 14:12:26 -0500

    Add files via upload

--- code/ch5/table_5-9/table_5-9.R
+++ code/ch5/table_5-9/table_5-9.R
@@ -204,7 +204,6 @@ Paths_NoInteraction <-
     data = Brader,
     boot_reps = 2000,
     boot_seed = boot_seed,
-    boot_parallel = "no",
     out_ipw = FALSE
   )$summary_df
 
@@ -248,7 +247,6 @@ Paths_DMInteraction <-
     data = Brader,
     boot_reps = 2000,
     round_decimal = 3,
-    boot_parallel = "no",
     boot_seed = boot_seed,
     out_ipw = FALSE
   )$summary_df
@@ -298,7 +296,6 @@ Paths_DMCInteraction <-
     D_model = NULL,
     data = Brader,
     boot_reps = 2000,
-    boot_parallel = "no",
     boot_seed = boot_seed,
     out_ipw = FALSE
   )$summary_df
